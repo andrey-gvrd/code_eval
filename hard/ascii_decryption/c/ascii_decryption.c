9bed230777ab14b38776b55f3207dd31135ea81c
andrey-gvrd <andrey.gvrd@gmail.com>
2014-12-21 18:40:19 +0300

    solve ascii_decryption challenge

--- hard/ascii_decryption/c/ascii_decryption.c
+++ hard/ascii_decryption/c/ascii_decryption.c
@@ -4,12 +4,14 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_LINES           (16)
+#define MAX_LINES           (8)
 #define MAX_VALUES          (512)
 #define MAX_CHARS_IN_NUMBER (3)
 #define MAX_LINE_LENGTH     ((MAX_CHARS_IN_NUMBER + 1) * MAX_VALUES)
 
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define NUM_ROWS(m) (sizeof(m) / sizeof(m[0]))
+#define NUM_COLS(m) (sizeof(m[0]) / sizeof(m[0][0]))
 
 typedef enum bool_e 
 {
@@ -19,16 +21,16 @@ typedef enum bool_e
 
 /**
  * Algorithm:
- * Find out which number represents a space, find number sequences of desired 
- * length which are surrounded by spaces (hopefully there's only one), figure
- * out the value with which the sequence is encoded, decode the sequence and 
- * print it out.
+ * Find out which number represents a space, find repeating number sequences 
+ * of desired length which are surrounded by spaces (hopefully there's only one)
+ * pair, figure out the value with which the sequence is encoded, decode the
+ * sequence and print it out.
 */
 
 /* Parsing function */
 static void createNumberMatrix(const char *fileName,
                                uint32_t matrix[][MAX_VALUES], uint32_t *columnCnt, uint32_t *rowCnt,
-                               uint32_t *sequenceLength, char *knownChar)
+                               uint32_t *seqLen, char *knownChar)
 {
     FILE *file = fopen(fileName, "r");
     char line[MAX_LINE_LENGTH];
@@ -36,7 +38,7 @@ static void createNumberMatrix(const char *fileName,
     *rowCnt = 0;
     while (fgets(line, MAX_LINE_LENGTH, file)) {
         /* Find the first value - length of the repeating sequence */
-        *sequenceLength = atoi(line);
+        *seqLen = atoi(line);
         /* Find the second value - a known charachter */
         p = strstr(line, "|");
         uint32_t i = p - &line[0] + 2;
@@ -94,73 +96,59 @@ static uint32_t findSpace(const uint32_t array[], const uint32_t arrayLength)
     return minValue;
 }
 
-int32_t main(int32_t argc, const char *argv[]) 
+static void matrixInit(uint32_t matrix[][MAX_VALUES], 
+                       const uint32_t rowCnt, const uint32_t columnCnt)
 {
-    uint32_t matrix[MAX_LINES][MAX_VALUES];
-    for (uint32_t i = 0; i < MAX_LINES; i++) {
-        for (uint32_t j = 0; j < MAX_VALUES; j++) {
+    for (uint32_t i = 0; i < rowCnt; i++) {
+        for (uint32_t j = 0; j < columnCnt; j++) {
+            //if (rowCnt == 32) { printf("m[%d][%d]: %d\n", i, j, matrix[i][j]); }
             matrix[i][j] = 0;
         }
     }
+}
+
+int32_t main(int32_t argc, const char *argv[]) 
+{
+    uint32_t matrix[MAX_LINES][MAX_VALUES];
+    matrixInit(matrix, NUM_ROWS(matrix), NUM_COLS(matrix));
     uint32_t columnCnt[MAX_LINES] = {0};
     uint32_t rowCnt = 0;
-    uint32_t sequenceLength = 0;
+    uint32_t seqLen = 0;
     char knownChar = 0;
     /* Extract the integer sequence into an array */
     createNumberMatrix(argv[1], matrix, &columnCnt[0], &rowCnt,
-                       &sequenceLength, &knownChar);
-    //printf("length: %d, charachter: %c\n", sequenceLength, knownChar);
+                       &seqLen, &knownChar);
+    //printf("seqLen: %d, knownChar: %c\n", seqLen, knownChar);
     /* Go through every input sequence */
     for (uint32_t i = 0; i < rowCnt; i++) {
-        int32_t codeDifference;
-        /* Find the most freqent number in the number sequence*/
-        uint32_t spaceCode = mostFrequentNumber(matrix[i], 255);
-        //printf("space is encoded as: %d\n", spaceCode);
-        /* Through every number sequence */
-        for (uint32_t j = 0; j < columnCnt[i] - 2 * sequenceLength + 1; j++) {
-            bool spaceFound = false;
-            uint32_t sequence[sequenceLength];
-            //printf("sequence: ");
-            /* Through every sequence of constant length */
-            for (uint32_t k = 0; k < sequenceLength; k++) {
-                if (matrix[i][j + k] == spaceCode) {
-                    spaceFound = true;
+        uint32_t spaceCode = findSpace(matrix[i], 255);
+        //printf("spaceCode: %d\n", spaceCode);
+        uint32_t potentSeqCnt = 0;
+        uint32_t potentSeq[16][seqLen];
+        matrixInit(potentSeq, NUM_ROWS(potentSeq), NUM_COLS(potentSeq));
+        for (uint32_t j = 0; j < columnCnt[i] - seqLen; j++) {
+            if ((matrix[i][j] == spaceCode) && 
+                (matrix[i][j + seqLen + 1] == spaceCode)) {
+                //printf("potentSeq index: %d, seq: ", j + 1);
+                /* memcpy doesn't copy the whole sequence for some reason */
+                //memcpy(&potentSeq[potentSeqCnt][0], &matrix[i][j + 1], seqLen);
+                for (uint32_t k = 0; k < seqLen; k++) {
+                    potentSeq[potentSeqCnt][k] = matrix[i][j + 1 + k];
+                    //printf("%d ", potentSeq[potentSeqCnt][k]);
                 }
-                sequence[k] = matrix[i][j + k];
-                //printf("%d ", sequence[k]);
+                //printf("\n");
+                potentSeqCnt++;
             }
-            //printf("\n");
-            if (spaceFound) {
-                continue;
-            }
-            /* Try to find that sequence in the rest of the number sequence */
-            for (uint32_t m = j + sequenceLength; m < columnCnt[i] - sequenceLength + 1; m++) {
-                uint32_t n = 0;
-                uint32_t overlap = 0;
-                while (matrix[i][m++] == sequence[n++]) {
-                    if (matrix[i][m - 1] == spaceCode) {
-                        //printf("space found, breaking\n");
-                        break;
-                    }
-                    //printf("%d == %d\n", matrix[i][m - 1], sequence[n - 1]);
-                    //printf("overlap: %d\n", overlap);
-                    overlap++;
-                }
-                if (overlap >= sequenceLength) {
-                    printf("sequence originally starting at %d found again at: %d\n", j, m - sequenceLength);
-                    codeDifference = sequence[sequenceLength - 1] - knownChar;
-                    //printf("codeDifference: %d\n", codeDifference);
-                } else {
-                    m--;
-                    n--;
-                    //printf("%d != %d\n", matrix[i][m], sequence[n]);
-                    m -= overlap;   // reset the index to previous state
-                }
+        }
+        uint32_t codeDiff = 0;
+        for (uint32_t j = 0; j < potentSeqCnt - 1; j++) {
+            if (!memcmp(&potentSeq[j][0], &potentSeq[j + 1][0], seqLen)) {
+                codeDiff = potentSeq[j][seqLen - 1] - knownChar;
+                //printf("codeDiff: %d\n", codeDiff);
             }
         }
         for (uint32_t j = 0; j < columnCnt[i]; j++) {
-            matrix[i][j] -= codeDifference;
-            printf("%c", matrix[i][j]);
+            printf("%c", matrix[i][j] - codeDiff);
         }
         printf("\n");
     }
