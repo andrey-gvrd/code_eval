37b4d6e5001cb64bcccd64746788dad29b942f75
andrey-gvrd <andrey.gvrd@gmail.com>
2015-01-04 16:16:45 +0300

    add string descriptions for enums use single type containing coordinates and type wherever possible

--- hard/ray_of_light/c/src/ray_of_light.c
+++ hard/ray_of_light/c/src/ray_of_light.c
@@ -4,39 +4,38 @@
 #include <ctype.h>  // isdigit
 
 #define ROOM_DIMENSIONS     (10)
+#define MAX_DISTANCE        (9)
 
 typedef enum {
     false = 0,
-    true = !false
+    true = 1
 } bool;
 
-typedef enum {
-    ray225 = 0,
-    ray45 = 1,
-    space = 2,
-    column = 3,
-    prism = 4,
-    wall = 5
+static const char *BOOL_STR[] = {
+    "false", "true"
+};
+
+typedef enum { 
+    ray225 = 0, ray45 = 1, space = 2, column = 3, prism = 4, wall = 5 
 } Elements_t;
 
+static const char *ELEMENTS_STR[] = {
+    "ray225", "ray45", "space", "column", "prism", "wall"
+};
+
 typedef enum {
-    no = 0,
-    left = 1,
-    top = 2,
-    right = 3,
-    bottom = 4
+    no = 0, left = 1, top = 2, right = 3, bottom = 4
 } EntrySide_t;
 
-typedef struct {
-    uint8_t x;
-    uint8_t y;
-} Point_t;
+static const char *ENTRY_SIDE_STR[] = {
+    "no", "left", "top", "right", "bottom"
+};
 
 typedef struct {
     uint8_t x;
     uint8_t y;
     Elements_t type;
-}
+} Point_t;
 
 static void clearArray(char array[][ROOM_DIMENSIONS]);
 static void printArray(char array[][ROOM_DIMENSIONS]);
@@ -44,7 +43,7 @@ static void printArray(char array[][ROOM_DIMENSIONS]);
 static Elements_t identify(char c)
 {
     Elements_t elementType = space;
-    printf("Identifying %c\n", c);
+    printf("Identifying '%c'\n", c);
     switch (c) {
         case '#': {
             elementType = wall;
@@ -139,61 +138,87 @@ static Point_t nextCoordinatesForEntry(Point_t point, Elements_t rayType,
     return nextPoint;
 }
 
-static Point_t nextCoordinates(char room[][ROOM_DIMENSIONS], 
-                               Point_t point, Elements_t rayType)
+static Point_t getActivePoint(char room[][ROOM_DIMENSIONS], Point_t point)
 {
     /* Find direction by looking at both potential point */
     /* Active point is a point with which the ray can interact:
        different ray, column, prism, wall */
     Point_t activePoint = { .x = 0, .y = 0 };
-    if (rayType = ray45) {
-        Point_t topRightPoint = { .x = point.x + 1, .y = point.y - 1 };
-        Point_t bottomLeftPoint = { .x = point.x - 1, .y = point.y + 1 };
-        printf("TR: [%d][%d], BL: [%d][%d]\n", topRightPoint.x, topRightPoint.y,
-            bottomLeftPoint.x, bottomLeftPoint.y);
+    if (point.type = ray45) {
+        Point_t topRightPoint = { .x = point.x + 1, 
+                                  .y = point.y - 1 };
+        Point_t bottomLeftPoint = { .x = point.x - 1, 
+                                    .y = point.y + 1 };
+        printf("TR: [%d][%d]; BL: [%d][%d]\n", 
+            topRightPoint.x, topRightPoint.y, bottomLeftPoint.x, bottomLeftPoint.y);
         /* Checking the top right point */
         if (isWithinBounds(topRightPoint)) {
             printf("Top right point is within bounds\n");
-            Elements_t topRightElement = identify(room[topRightPoint.y][topRightPoint.x]);
-            if (topRightElement != ray45) {
+            topRightPoint.type = identify(room[topRightPoint.y][topRightPoint.x]);
+            printf("TR: %s\n", ELEMENTS_STR[topRightPoint.type]);
+            if (topRightPoint.type != ray45) {
                 printf("Top right point is an active point\n");
                 activePoint.x = topRightPoint.x;
                 activePoint.y = topRightPoint.y;
+                activePoint.type = topRightPoint.type;
             }
         }
         /* Checking the bottom left point */
         if (isWithinBounds(bottomLeftPoint)) {
             printf("Bottom left point is within bounds\n");
-            Elements_t bottomLeftElement = identify(room[bottomLeftPoint.y][bottomLeftPoint.x]);
-            if (bottomLeftElement != ray45) {
+            bottomLeftPoint.type = identify(room[bottomLeftPoint.y][bottomLeftPoint.x]);
+            printf("BL: %s\n", ELEMENTS_STR[bottomLeftPoint.type]);
+            if (bottomLeftPoint.type != ray45) {
                 printf("Bottom left point is an active point\n");
                 activePoint.x = bottomLeftPoint.x;
                 activePoint.y = bottomLeftPoint.y;
+                activePoint.type = bottomLeftPoint.type;
             }
         }
-        Elements_t activePointType = identify(room[activePoint.y][activePoint.x]);
-        printf("Active point coordinates: [%d][%d], type: %d\n", 
-            activePoint.x, activePoint.y, activePointType);
-    } else if (rayType = ray225) {
+        printf("Active point: [%d][%d], %s\n", 
+            activePoint.x, activePoint.y, ELEMENTS_STR[activePoint.type]);
+    } else if (point.type = ray225) {
         /* */
     }
     return activePoint;
 }
 
-static char getNextCharacter(Elements_t elementType, Elements_t nextElementType)
+static char getNextCharacter(Elements_t currentElement, Elements_t nextElement)
 {
     char nextChar = 'N';
-    switch(nextElementType) {
+    printf("Current: %s, Next: %s\n", 
+        ELEMENTS_STR[currentElement], ELEMENTS_STR[nextElement]);
+    switch(nextElement) {
         case space: {
-            if (elementType == ray45) {
+            if (currentElement == ray45) {
                 nextChar = '/';
-            } else if ((elementType == ray225)) {
+            } else if ((currentElement == ray225)) {
                 nextChar = '\\';
             }
             break;
         }
     }
     return nextChar;
+}
+
+static Point_t getEntryPoint(char room[][ROOM_DIMENSIONS])
+{
+    Point_t entryPoint = { .x = 0, .y = 0 };
+    bool entryFound = false;
+    for (uint8_t i = 0; i < ROOM_DIMENSIONS && !entryFound; ++i) {
+        for (uint8_t j = 0; j < ROOM_DIMENSIONS && !entryFound; ++j) {
+            entryPoint.type = identify(room[i][j]);
+            if ((entryPoint.type == ray45) ||
+                (entryPoint.type == ray225)) {
+                entryPoint.x = j;
+                entryPoint.y = i;
+                entryFound = true;
+            }
+        }
+    }
+    printf("Entry point: [%d][%d], %s\n", 
+        entryPoint.x, entryPoint.y, ELEMENTS_STR[entryPoint.type]);
+    return entryPoint;
 }
 
 int32_t main(int32_t argc, const char *argv[]) 
@@ -207,35 +232,22 @@ int32_t main(int32_t argc, const char *argv[])
         if (lineCnt == ROOM_DIMENSIONS) {
             printArray(room);
             /* Find first ray */
-            Point_t entryPoint = { .x = 0, .y = 0 };
-            Elements_t elementType;
-            bool entryFound = false;
-            for (uint8_t i = 0; i < ROOM_DIMENSIONS && !entryFound; ++i) {
-                for (uint8_t j = 0; j < ROOM_DIMENSIONS && !entryFound; ++j) {
-                    elementType = identify(room[i][j]);
-                    if ((elementType == ray45) ||
-                        (elementType == ray225)) {
-                        entryPoint.x = j;
-                        entryPoint.y = i;
-                        entryFound = true;
-                    }
-                }
-            }
-            printf("Entry: [%d][%d]\n", entryPoint.x, entryPoint.y);
+            Point_t entryPoint = getEntryPoint(room);
             /* Replace cells with appropriate rays */
             uint8_t travelledDistance = 0;
-            Point_t currentPoint = { .x = entryPoint.x, .y = entryPoint.y };
-            while (travelledDistance < 20) {
-                Point_t nextPoint = nextCoordinates(room, 
-                                                    currentPoint, elementType);
-                printf("Next point: [%d][%d]\n", nextPoint.x, nextPoint.y);
-                Elements_t nextPointType = identify(room[nextPoint.y][nextPoint.x]);
-                printf("Next point type: %d\n", nextPointType);
-                char nextChar = getNextCharacter(elementType, nextPointType);
+            Point_t currentPoint = { .x = entryPoint.x, .y = entryPoint.y,
+                                     .type = entryPoint.type };
+            while (travelledDistance < MAX_DISTANCE) {
+                Point_t nextPoint = getActivePoint(room, currentPoint);
+                printf("Pre-Current: %s, Pre-Next: %s\n", 
+                    ELEMENTS_STR[currentPoint.type], ELEMENTS_STR[nextPoint.type]);
+                char nextChar = getNextCharacter(currentPoint.type, nextPoint.type);
                 room[nextPoint.y][nextPoint.x] = nextChar;
+                nextPoint.type = identify(nextChar);
                 printArray(room);
                 currentPoint.x = nextPoint.x;
                 currentPoint.y = nextPoint.y;
+                currentPoint.type = nextPoint.type;
                 travelledDistance++;
             }
             clearArray(room);
